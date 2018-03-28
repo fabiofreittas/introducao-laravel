e48382e3057baef37058d8b3ac452b9a916bea6a
Fábio Freitas <fabio.freitas@unir.br>
2018-03-27 22:23:02 -0400

    Aula 27/03/2018

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/produtos/{descricao?}', function ($descricao = null){
+    return "<h1>Descrição: {$descricao}</h1>";
+});
+
+Route::get('/usuarios', function (){
+    return "<h1>Listagem de Usuários</h1>";
+});
